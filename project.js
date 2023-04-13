77728665eda21254acc599f880daf23b660c2a08
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-04-13 10:40:20 +0300

    Deposit some money function is ready

--- project.js
+++ project.js
@@ -7,3 +7,21 @@
 //7. play again
 
 // 7 steps logic behind the slot app
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const depositAmount = deposit();
+
