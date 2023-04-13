639b788b95f4f7567a8337e2788f5941760ac7aa
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-04-13 08:49:38 +0300

    7 steps logic behind the slot app

---
+++ project.js
@@ -0,0 +1,9 @@
+//1. Desposit some money
+//2. Determine number of lines to bet on
+//3. Collect bet amouth
+//4.Spin the slot machine
+//5.Check if the user won
+//6. Give the user their winnings
+//7. play again
+
+// 7 steps logic behind the slot app
